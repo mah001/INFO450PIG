1848a11a1db7a6dd37e19cb8578e49208d5bf02a
mah001 <mahmoodim@vcu.edu>
2017-10-12 21:10:12 -0700

    pLatin

--- pLatin/pLatin.cpp
+++ pLatin/pLatin.cpp
@@ -4,8 +4,45 @@
 #include "stdafx.h"
 
 
+#include <iostream>
+#include <string>
+using namespace std;
+string PigLatin(string word);
+bool vowelWord(char charactor);
+string word;
 int main()
 {
-    return 0;
+
+	do {
+		cout << "To exit type 'Quit' or " << "Please enter a word to translate into Pig Latin: ";
+		cin >> word;
+
+		cout << "That translates to: "
+			<< PigLatin(word) << endl;
+
+	}
+
+	while (word != "Quit");
+
+	return 0;
 }
+
+string PigLatin(string word)
+
+{
+	if (vowelWord(word.at(0))) {
+		return word + "ay";
+	}
+	else {
+		return word.substr(1, word.length() - 1) +
+			word.at(0) + "ay";
+	}
+}
+bool vowelWord(char charactor)
+
+{
+	const string VOWELS = "aeiouAEIOU";
+	return (VOWELS.find(charactor) != string::npos);
+}
+
 
